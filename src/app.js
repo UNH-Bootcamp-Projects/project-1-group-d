40ab405b26ba95d7a572049d23d084db524bec4b
theeshyguy <t.siopes00@yahoo.com>
2021-04-17 10:34:25 -0400

    working file

---
+++ src/app.js
@@ -0,0 +1,15 @@
+let heroInput;
+
+function getMarvelApi () {
+    // event.preventDefault();
+    let apikey = "8dc274afb84abf0f19f28c01d6ac7425";
+    let requestUrl = "https://gateway.marvel.com:443/v1/public/characters?name=" + heroInput + "&orderBy=name&apikey=" + apikey + "&ts=1&hash=6262a02cba8e28cbd51f531c1e20a49f";
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+      console.log(data.data.results[0].description);
+    })
+}
